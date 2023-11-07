75040aba9ae78671ddafd2dff372bc10d57b1adc
Bassel-saleh <basselh26@gmail.com>
2023-11-07 11:37:15 -0800

    Updated

---
+++ alx_shell/shell.h
@@ -0,0 +1,14 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+#define MAX_AR_GS 60
+#define MAX_LINE_LEN_GTH 1024
+
+#endif
