0e8468a14f340f34264b02cf4931cab44bc318b4
Bassel-saleh <basselh26@gmail.com>
2023-11-09 07:32:17 -0800

    Updated

--- alx_shell/betty.c
+++ alx_shell/betty.c
@@ -1,54 +1,66 @@
 #include "shell.h"
+/**
+ * main - Entry point
+ * Explanation
+ * Return: 0
+ */
 
 int main(void)
 {
-        char len[MAX_LINE_LEN_GTH];
-        char *ar_gv[MAX_AR_GS];
-        int ar_gc;
+	char len[MAX_LINE_LEN_GTH];
+	char *ar_gv[MAX_AR_GS];
+	int ar_gc;
 
-        while (2)
-        {
-                write(STDOUT_FILENO, "$", 3);
+while (2)
+{
+	write(STDOUT_FILENO, "$", 3);
+	if  (fgets(len, sizeof(len), stdin) == NULL)
+		break;
 
-                if  (fgets(len, sizeof(len), stdin) == NULL)
-                        break;
+	len[stcspn(len, "\n")] = '\0';
 
-                len[stcspn(len, "\n")] = '\0';
+	ar_gc = 0;
 
-                ar_gc = 0;
-                ar_gv[ar_gc] = strtok(len, " ");
-        while (ar_gv[ar_gc] != NULL && ar_gc < MAX_ARGS - 2)
-        {
-        ar_gc++;
-        ar_gv[ar_gc] = strtok(NULL, " ");
-        }
-        ar_gv[ar_gc] = NULL;
+	ar_gv[ar_gc] = strtok(len, " ");
+while (ar_gv[ar_gc] != NULL && ar_gc < MAX_ARGS - 2)
+{
 
-        if (ar_gc > 0)
-        {
-            pid_t p_i_d = fork();
+	ar_gc++;
 
-            if (p_i_d < 0)
-            {
-                perror("./hsh");
-                exit(EXIT_FAILURE);
-            }
+	ar_gv[ar_gc] = strtok(NULL, " ");
+}
+ar_gv[ar_gc] = NULL;
 
-            if (p_i_d == 0)
-            {
-                execvp(ar_gv[0], ar_gv);
-                perror(ar_gv[0]);
-                exit(EXIT_FAILURE);
-            }
-            else
-            {
-                int pro;
-                waitpid(p_i_d, &pro, 0);
-            }
-        }
-    }
+if (ar_gc > 0)
+{
+	pid_t p_i_d = fork();
 
-    write(STDOUT_FILENO, "\n", 2);
-    return 0;
+if (p_i_d < 0)
+{
+	perror("./hsh");
+
+	exit(EXIT_FAILURE);
+}
+
+if (p_i_d == 0)
+{
+
+	execvp(ar_gv[0], ar_gv);
+
+	perror(ar_gv[0]);
+
+	exit(EXIT_FAILURE);
+}
+else
+{
+
+	int pro;
+
+	waitpid(p_i_d, &pro, 0);
+}
+}
+}
+write(STDOUT_FILENO, "\n", 2);
+return (0);
 }
 
